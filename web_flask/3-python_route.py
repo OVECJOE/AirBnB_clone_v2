0a5cc3fb3fc075749e4828657f7e61b03b71083f
OVECJOE <ovecjoe123@gmail.com>
2022-02-03 21:10:32 +0100

    Task 3 done :tada:

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A script that starts a web application"""
+from re import L
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C " + " ".join(text.split("_"))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    return "Python " + " ".join(text.split("_"))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
